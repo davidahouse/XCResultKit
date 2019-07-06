ebb482d1fed1cd553c29e362a3905932d9e65c59
David House <davidahouse@gmail.com>
2019-07-06 18:30:15 -0400

    :sparkles: New XCResultFile class

--- Sources/XCResultKit/TestFailureIssueSummary.swift
+++ Sources/XCResultKit/TestFailureIssueSummary.swift
@@ -14,10 +14,18 @@ import Foundation
 
 public struct TestFailureIssueSummary: XCResultObject {
     public let testCaseName: String
+    public let issueType: String
+    public let message: String
+    public let producingTarget: String?
+    public let documentLocationInCreatingWorkspace: DocumentLocation?
     
     public init?(_ json: [String : AnyObject]) {
         do {
             testCaseName = try xcRequired(element: "testCaseName", from: json)
+            issueType = try xcRequired(element: "issueType", from: json)
+            message = try xcRequired(element: "message", from: json)
+            producingTarget = xcOptional(element: "producingTarget", from: json)
+            documentLocationInCreatingWorkspace = xcOptional(element: "documentLocationInCreatingWorkspace", from: json)
         } catch {
             print("Error parsing ResultMetrics: \(error.localizedDescription)")
             return nil
---
+++ Sources/XCResultKit/XCResultFile.swift
@@ -0,0 +1,82 @@
+//
+//  XCResultFile.swift
+//  
+//
+//  Created by David House on 7/6/19.
+//
+
+import Foundation
+
+class XCResultFile {
+    
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func getInvocationRecord() -> ActionsInvocationRecord? {
+        
+        guard let getOutput = shell(command: ["-l", "-c", "xcrun xcresulttool get --path \(url.path) --format json"]) else {
+            return nil
+        }
+        
+        do {
+            guard let data = getOutput.data(using: .utf8) else {
+                print("Unable to turn string into data, must not be a utf8 string")
+                return nil
+            }
+            
+            guard let rootJSON = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: AnyObject] else {
+                print("Expecting top level dictionary but didn't find one")
+                return nil
+            }
+            
+            let invocation = ActionsInvocationRecord(rootJSON)
+            return invocation
+        } catch {
+            print("Error deserializing JSON: \(error)")
+            return nil
+        }
+    }
+    
+    func getTestPlanRunSummaries(id: String) -> ActionTestPlanRunSummaries? {
+        
+        guard let getOutput = shell(command: ["-l", "-c", "xcrun xcresulttool get --path \(url.path) --id \(id) --format json"]) else {
+            return nil
+        }
+        
+        do {
+            guard let data = getOutput.data(using: .utf8) else {
+                print("Unable to turn string into data, must not be a utf8 string")
+                return nil
+            }
+            
+            guard let rootJSON = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: AnyObject] else {
+                print("Expecting top level dictionary but didn't find one")
+                return nil
+            }
+            
+            let runSummaries = ActionTestPlanRunSummaries(rootJSON)
+            return runSummaries
+        } catch {
+            print("Error deserializing JSON: \(error)")
+            return nil
+        }
+    }
+    
+    private func shell(command: [String]) -> String? {
+        let task = Process()
+        task.launchPath = "/bin/sh"
+        task.arguments = command
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String? = String(data: data, encoding: String.Encoding.utf8)
+        
+        return output
+    }
+}
